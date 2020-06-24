460b700b5f6997e1fc8a9a7bfb76f64f8a2ac778
MSI <312410191@qq.com>
2020-06-24 15:06:08 +0800

    fix

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         library: 'ccDirective',
+        libraryTarget: 'umd',
         path: path.resolve(__dirname, 'dist'),
         filename: 'ccDirective.js'
     },
